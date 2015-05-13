2dffc59001fb4b5c8561619aa33cc2be125d0391
Devin Johnson <devin.johnson@noaa.gov>
2015-05-13 09:47:10 -0700

    Added new license file

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -10,98 +10,78 @@ using namespace Rcpp;
 arma::mat makeT(const double& b, const double& delta);
 RcppExport SEXP crawl_makeT(SEXP bSEXP, SEXP deltaSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< const double& >::type b(bSEXP );
-        Rcpp::traits::input_parameter< const double& >::type delta(deltaSEXP );
-        arma::mat __result = makeT(b, delta);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< const double& >::type b(bSEXP);
+    Rcpp::traits::input_parameter< const double& >::type delta(deltaSEXP);
+    __result = Rcpp::wrap(makeT(b, delta));
+    return __result;
 END_RCPP
 }
 // makeQ
 arma::mat makeQ(const double& b, const double& sig2, const double& delta);
 RcppExport SEXP crawl_makeQ(SEXP bSEXP, SEXP sig2SEXP, SEXP deltaSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< const double& >::type b(bSEXP );
-        Rcpp::traits::input_parameter< const double& >::type sig2(sig2SEXP );
-        Rcpp::traits::input_parameter< const double& >::type delta(deltaSEXP );
-        arma::mat __result = makeQ(b, sig2, delta);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< const double& >::type b(bSEXP);
+    Rcpp::traits::input_parameter< const double& >::type sig2(sig2SEXP);
+    Rcpp::traits::input_parameter< const double& >::type delta(deltaSEXP);
+    __result = Rcpp::wrap(makeQ(b, sig2, delta));
+    return __result;
 END_RCPP
 }
 // CTCRWNLL
 Rcpp::List CTCRWNLL(const arma::mat& y, const arma::mat& Hmat, const arma::mat& Qmat, const arma::mat& Tmat, const arma::vec& noObs, const arma::vec& active, const arma::colvec& a, const arma::mat& P);
 RcppExport SEXP crawl_CTCRWNLL(SEXP ySEXP, SEXP HmatSEXP, SEXP QmatSEXP, SEXP TmatSEXP, SEXP noObsSEXP, SEXP activeSEXP, SEXP aSEXP, SEXP PSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< const arma::mat& >::type y(ySEXP );
-        Rcpp::traits::input_parameter< const arma::mat& >::type Hmat(HmatSEXP );
-        Rcpp::traits::input_parameter< const arma::mat& >::type Qmat(QmatSEXP );
-        Rcpp::traits::input_parameter< const arma::mat& >::type Tmat(TmatSEXP );
-        Rcpp::traits::input_parameter< const arma::vec& >::type noObs(noObsSEXP );
-        Rcpp::traits::input_parameter< const arma::vec& >::type active(activeSEXP );
-        Rcpp::traits::input_parameter< const arma::colvec& >::type a(aSEXP );
-        Rcpp::traits::input_parameter< const arma::mat& >::type P(PSEXP );
-        Rcpp::List __result = CTCRWNLL(y, Hmat, Qmat, Tmat, noObs, active, a, P);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< const arma::mat& >::type y(ySEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type Hmat(HmatSEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type Qmat(QmatSEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type Tmat(TmatSEXP);
+    Rcpp::traits::input_parameter< const arma::vec& >::type noObs(noObsSEXP);
+    Rcpp::traits::input_parameter< const arma::vec& >::type active(activeSEXP);
+    Rcpp::traits::input_parameter< const arma::colvec& >::type a(aSEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type P(PSEXP);
+    __result = Rcpp::wrap(CTCRWNLL(y, Hmat, Qmat, Tmat, noObs, active, a, P));
+    return __result;
 END_RCPP
 }
 // CTCRWPREDICT
 Rcpp::List CTCRWPREDICT(const arma::mat& y, const arma::mat& Hmat, const arma::mat& Qmat, const arma::mat& Tmat, const arma::vec& noObs, const arma::vec& active, const arma::colvec& a, const arma::mat& P);
 RcppExport SEXP crawl_CTCRWPREDICT(SEXP ySEXP, SEXP HmatSEXP, SEXP QmatSEXP, SEXP TmatSEXP, SEXP noObsSEXP, SEXP activeSEXP, SEXP aSEXP, SEXP PSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< const arma::mat& >::type y(ySEXP );
-        Rcpp::traits::input_parameter< const arma::mat& >::type Hmat(HmatSEXP );
-        Rcpp::traits::input_parameter< const arma::mat& >::type Qmat(QmatSEXP );
-        Rcpp::traits::input_parameter< const arma::mat& >::type Tmat(TmatSEXP );
-        Rcpp::traits::input_parameter< const arma::vec& >::type noObs(noObsSEXP );
-        Rcpp::traits::input_parameter< const arma::vec& >::type active(activeSEXP );
-        Rcpp::traits::input_parameter< const arma::colvec& >::type a(aSEXP );
-        Rcpp::traits::input_parameter< const arma::mat& >::type P(PSEXP );
-        Rcpp::List __result = CTCRWPREDICT(y, Hmat, Qmat, Tmat, noObs, active, a, P);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< const arma::mat& >::type y(ySEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type Hmat(HmatSEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type Qmat(QmatSEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type Tmat(TmatSEXP);
+    Rcpp::traits::input_parameter< const arma::vec& >::type noObs(noObsSEXP);
+    Rcpp::traits::input_parameter< const arma::vec& >::type active(activeSEXP);
+    Rcpp::traits::input_parameter< const arma::colvec& >::type a(aSEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type P(PSEXP);
+    __result = Rcpp::wrap(CTCRWPREDICT(y, Hmat, Qmat, Tmat, noObs, active, a, P));
+    return __result;
 END_RCPP
 }
 // CTCRWSAMPLE
 Rcpp::List CTCRWSAMPLE(const arma::mat& y, const arma::mat& Hmat, const arma::mat& Qmat, const arma::mat& Tmat, const arma::vec& noObs, const arma::vec& active, const arma::colvec& a, const arma::mat& P);
 RcppExport SEXP crawl_CTCRWSAMPLE(SEXP ySEXP, SEXP HmatSEXP, SEXP QmatSEXP, SEXP TmatSEXP, SEXP noObsSEXP, SEXP activeSEXP, SEXP aSEXP, SEXP PSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< const arma::mat& >::type y(ySEXP );
-        Rcpp::traits::input_parameter< const arma::mat& >::type Hmat(HmatSEXP );
-        Rcpp::traits::input_parameter< const arma::mat& >::type Qmat(QmatSEXP );
-        Rcpp::traits::input_parameter< const arma::mat& >::type Tmat(TmatSEXP );
-        Rcpp::traits::input_parameter< const arma::vec& >::type noObs(noObsSEXP );
-        Rcpp::traits::input_parameter< const arma::vec& >::type active(activeSEXP );
-        Rcpp::traits::input_parameter< const arma::colvec& >::type a(aSEXP );
-        Rcpp::traits::input_parameter< const arma::mat& >::type P(PSEXP );
-        Rcpp::List __result = CTCRWSAMPLE(y, Hmat, Qmat, Tmat, noObs, active, a, P);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< const arma::mat& >::type y(ySEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type Hmat(HmatSEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type Qmat(QmatSEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type Tmat(TmatSEXP);
+    Rcpp::traits::input_parameter< const arma::vec& >::type noObs(noObsSEXP);
+    Rcpp::traits::input_parameter< const arma::vec& >::type active(activeSEXP);
+    Rcpp::traits::input_parameter< const arma::colvec& >::type a(aSEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type P(PSEXP);
+    __result = Rcpp::wrap(CTCRWSAMPLE(y, Hmat, Qmat, Tmat, noObs, active, a, P));
+    return __result;
 END_RCPP
 }
