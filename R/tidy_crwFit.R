ec4fb3b1751338882182f5b90666f65338826397
Josh London <josh.london@noaa.gov>
2017-02-26 11:17:26 -0800

    ADD tidy-like METHOD FOR crwFit
    tidy_crwFit() returns a data.frame of key parameters from a crwFit object. Seems I should have been able to add this as tidy.crwFit method so one could call tidy(my_crwFit_obj). But, couldn't figure it out.

---
+++ R/tidy_crwFit.R
@@ -0,0 +1,31 @@
+#' tidy method for crwFit object
+#' @export
+tidy_crwFit <- function(fit) {
+  terms <- data.frame(term = fit$nms)
+  out <- as.data.frame(round(cbind(
+    fit$par, fit$se, fit$ci[, 1], fit$ci[, 2]), 3)
+  )
+  
+  colnames(out) <- c("estimate", 
+                     "std.error", 
+                     "conf.low", 
+                     "conf.high")
+  out <- cbind(terms,out)
+  
+  out <- rbind(out,
+               data.frame(
+                 term = "logLik",
+                 estimate = fit$loglik,
+                 std.error = NA,
+                 conf.low = NA,
+                 conf.high = NA
+               ),
+               data.frame(
+                 term = "AIC",
+                 estimate = fit$aic,
+                 std.error = NA,
+                 conf.low = NA,
+                 conf.high = NA
+               ))
+  out
+}
