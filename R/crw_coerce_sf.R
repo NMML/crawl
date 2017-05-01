8ceb3e90670679c0aab6894ca735068cd4c2ace5
Josh London <josh.london@noaa.gov>
2017-05-01 16:42:11 -0700

    COMBINE crw_as_sf FOR POINT & LINESTRING
    crw_as_sf now handles either POINT or LINESTRING coersion.

---
+++ R/crw_coerce_sf.R
@@ -0,0 +1,70 @@
+#' Coerce to sf/sfc object
+#' 
+#' @param crw_object an object of class \code{"crwIS"} or \code{"crwPredict"}
+#' @param epsg integer epsg code specifying the coordinate projection
+#' @param ftype character of either "POINT" or "LINESTRING" specifying the feature type
+#' @param group character specifying the column to group by for mulitple LINESTRING features
+#' @export
+
+crw_as_sf <- function(crw_object,epsg,ftype,group) {
+  UseMethod("crw_as_sf",crw_object)
+}
+
+#' @describeIn crw_as_sf coerce crwIS object to sf (POINT geometry) or sfc_LINESTRING
+#' @export 
+crw_as_sf.crwIS <- function(crw_object,epsg,ftype,group) {
+  if(!missing(group)) {
+    warning("group argument not applicable to crwIS objects. ignorning")
+  }
+  stopifnot(!missing(epsg))
+  stopifnot(!missing(ftype))
+  stopifnot(ftype %in% c("POINT","LINESTRING"))
+  if(ftype == "POINT") {
+  crw_object <- crw_as_tibble(crw_object) %>% 
+    sf::st_as_sf(coords = c("mu.x","mu.y")) %>% 
+    sf::st_set_crs(epsg) 
+  }
+  if(ftype == "LINESTRING") {
+    crw_object <- crw_as_tibble(crw_object) %>% 
+      sf::st_as_sf(coords = c("mu.x","mu.y")) %>% 
+      sf::st_set_crs(epsg) %>% 
+      sf::st_coordinates() %>% 
+      sf::st_linestring() %>% 
+      sf::st_sfc()
+  }
+  return(crw_object)
+}
+
+#' @describeIn crw_as_sf coerce crwPredict object to sf (POINT geometry) or sfc_LINESTRING
+#' @export
+crw_as_sf.crwPredict <- function(crw_object,epsg,ftype,group) {
+  stopifnot(!missing(epsg))
+  stopifnot(!missing(ftype))
+  stopifnot(ftype %in% c("POINT","LINESTRING"))
+  if(ftype == "POINT" && missing(group)) {
+  crw_object <- crw_as_tibble(crw_object) %>% 
+    sf::st_as_sf(coords = c("mu.x","mu.y")) %>% 
+    sf::st_set_crs(epsg) 
+  }
+  if(ftype == "POINT" && !missing(group)) {
+   warning("group argument not applicable for 'POINT' type. ignoring")
+  }
+  if(ftype == "LINESTRING" && missing(group)) {
+    crw_object <- crw_as_tibble(crw_object) %>% 
+      sf::st_as_sf(coords = c("mu.x","mu.y")) %>% 
+      sf::st_set_crs(epsg) %>% 
+      sf::st_coordinates() %>% 
+      sf::st_linestring() %>% 
+      sf::st_sfc()
+  }
+  if(ftype == "LINESTRING" && !missing(group)) {
+    crw_object <- crw_as_tibble(crw_object) %>% 
+      sf::st_as_sf(coords = c("mu.x","mu.y")) %>% 
+      sf::st_set_crs(epsg) %>% 
+      dplyr::group_by(group) %>% 
+      dplyr::summarize() %>% 
+      sf::st_cast("LINESTRING") %>% 
+      sf::st_sfc()
+  }
+  return(crw_object)
+}
